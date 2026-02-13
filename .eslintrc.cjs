63ab59a40c2c2a8babc6d7b99f9e54c5803350b6
Veronika Malyshkova <malyshkovav@mac-profi.ru>
2026-02-13 13:15:17 +0700

    chore: add ESLint configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "import"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  rules: {
+    "import/no-default-export": "error"
+  }
+};
