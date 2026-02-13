60127039b88db6241abf68b250800cd4e3deb20d
Veronika Malyshkova <malyshkovav@mac-profi.ru>
2026-02-13 14:11:25 +0700

    feat: add global error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import routes from "./routes/index.js";
 import type { Express } from "express";
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 export const app: Express = express();
 
@@ -9,3 +10,4 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/", routes);
+app.use(errorHandler);
