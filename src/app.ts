236592dd27d5ae17cdf848d5617ed886c6330052
Veronika Malyshkova <malyshkovav@mac-profi.ru>
2026-02-13 13:17:11 +0700

    refactor: adjust app/server structure after ESLint setup

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes/index.js";
+import type { Express } from "express";
 
-const app = express();
+export const app: Express = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/", routes);
-
-export default app;
